8f2edf16a3a3343eb008f3a59dcba873838faa55
mighuel lucas <mighuel-lucaas@hotmail.com>
2016-06-04 19:44:52 -0300

    wadasdas

--- MVCPiqueSysMarket/MVCPiqueSysMarket/DAL/MyContext.cs
+++ MVCPiqueSysMarket/MVCPiqueSysMarket/DAL/MyContext.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using MVCPiqueSysMarket.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace MVCPiqueSysMarket.DAL
 {
-    public class MyContext
+    public class MyContext : DbContext
     {
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Cliente> Usuarios { get; set; }
 
+        public MyContext() : base("Data Source=(localdb)\\v11.0;Initial Catalog=TrabalhoPO;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True;Application Name=EntityFramework")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
